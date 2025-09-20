0d2f45950559ca3a4f2d8aec1fdd1aeff9e53b35
zwjkf <503755414@qq.com>
2025-09-20 21:49:15 +0800

    update

--- assets/all-wallets-BBEy77VW.js
+++
@@ -1,6 +0,0 @@
-import{m as o}from"./core-yib9qb-W.js";import"./index-DhALtbbs.js";import"./index.es-DHN9XZmD.js";import"./index-nibyPLVP.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{r as allWalletsSvg};
